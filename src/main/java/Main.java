d7201c572dbbc29f9502d698fbe27dc01e310301
chlee1234 <zzaoman@naver.com>
2022-06-28 16:20:46 +0900

    8번 평균구하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,21 @@
-import java.util.*;
 public class Main {
-    public double solution(int[] arr) {
-        double answer  = Arrays.stream(arr).average().orElse(0);
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        int[] arr3 = new int[7]; //공부시간 계산 배열 선언
+        for (int i=0; i< 7; i++) {
+            if (arr2[i] >= 29) {  // check out 29시 이상이면 21시로 바꿔줌
+                arr2[i] = 21;
+            }
+            answer += arr2[i] - arr1[i];
+        }
 
         return answer;
     }
 
-
     public static void main(String[] args) {
-        int[] arr1 = new int [] {1,2,3,4,5};
-        int[] arr2 = new int [] {3,5,7,9};
-
-        Main result = new Main();
-        //아래는 테스트로 출력해 보기 위한 코드입니다.
-        System.out.println("결과 : " + result.solution(arr1));
-        System.out.println("결과 : " + result.solution(arr2));
+        Main method = new Main();
+        int[] arr1 = {9, 9, 8, 8, 7, 8, 9};
+        int[] arr2 = {21, 25, 30, 29, 22, 23, 30};
+        System.out.println(method.solution(arr1, arr2));
     }
 }
