ee09517ce224686500e9b0d10b79ec35c70c54d2
chlee1234 <zzaoman@naver.com>
2022-06-28 13:21:19 +0900

    8번 평균구하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+import java.util.*;
 public class Main {
+    public double solution(int[] arr) {
+        double answer  = Arrays.stream(arr).average().orElse(0);
+
+        return answer;
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr1 = new int [] {1,2,3,4,5};
+        int[] arr2 = new int [] {3,5,7,9};
+
+        Main result = new Main();
+        //아래는 테스트로 출력해 보기 위한 코드입니다.
+        System.out.println("결과 : " + result.solution(arr1));
+        System.out.println("결과 : " + result.solution(arr2));
     }
 }
